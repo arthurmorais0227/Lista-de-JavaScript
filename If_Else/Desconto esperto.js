46b28b9700ff19f4a0bb3fb686a9084194f281bf
Arthur Morais <mlkliso4000@gmail.com>
2025-03-28 16:21:57 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If_Else/Desconto esperto.js
@@ -0,0 +1,7 @@
+let salário = 4000;
+
+if (salário > 2000) {
+    console.log(`O salário após o desconto é de ${salário * 0.90}.`);
+}else {
+    console.log(`O salário após o desconto é de ${salário * 0.95}.`);
+}   
