d82dd17f0e4b668ea0127f976e4f96e21b50863f
Arthur Morais <mlkliso4000@gmail.com>
2025-03-28 11:14:57 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Operadores Lógicos/Maior que 100.js
@@ -0,0 +1,9 @@
+let num1 = 1;
+let num2 = 1;
+let num3 = 1;
+
+if (num1 > 100 || num2 > 100 || num3 > 100) {
+    console.log("Pelo menos um dos números fornecidos é maior que 100.");
+}else {
+    console.log("Nenhum dos números fornecidos é maior que 100.");
+}
