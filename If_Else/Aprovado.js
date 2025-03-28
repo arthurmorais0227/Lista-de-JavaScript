09e294f6c51d71315b349800dcc31315e14f189c
Arthur Morais <mlkliso4000@gmail.com>
2025-03-28 16:11:22 -0300

    feat(if-else): adiciona verificação de aprovação por nota

---
+++ If_Else/Aprovado.js
@@ -0,0 +1,10 @@
+let nota = 100;
+
+if (nota > 60 && nota <= 100) {
+    console.log("Aprovado!✅");
+}else if (nota <= 60 && nota >= 0) {
+    console.log("Reprovado!❌");
+}else {
+    console.log("Nota inválida!");
+}
+    
