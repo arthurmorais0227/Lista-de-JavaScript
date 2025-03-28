d012148ff404e4c8a8c7766e78bb71f9d43ecd76
Arthur Morais <mlkliso4000@gmail.com>
2025-03-28 11:19:13 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Operadores Lógicos/AND em ação.js
@@ -0,0 +1,8 @@
+let amoMinhaNamorada = false;
+let minhaNamoradaMeAma = true;
+
+if(amoMinhaNamorada == true && minhaNamoradaMeAma == true) {
+    console.log("Somos um casal feliz!");
+}else {
+    console.log("Não somos um casal feliz!");
+}
