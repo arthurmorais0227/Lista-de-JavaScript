49d1d8ee98d63ecd8ba84053a1151e124c499074
Arthur Morais <mlkliso4000@gmail.com>
2025-03-31 08:00:41 -0300

    feat(switch-case): adiciona exibição do dia da semana

---
+++ Switch_Case/Dia da semana.js
@@ -0,0 +1,27 @@
+let dia = 0;
+
+switch (dia) {
+  case dia = 1:
+    console.log("Domingo!");
+    break;
+  case dia = 2:
+    console.log("Segunda-feira!");
+    break;
+  case dia = 3:
+    console.log("Terça-feira!");
+    break;
+  case dia = 4:
+    console.log("Quarta-feira!");
+    break;
+  case dia = 5:
+    console.log("Quinta-feira!");
+    break;
+  case dia = 6:
+    console.log("Sexta-feira!");
+    break;
+  case dia = 7:
+    console.log("Sábado!");
+    break;
+  default:
+    console.log("Número inválido! Insira um número de 1 a 7.");
+}
