9d5e608e6c0de6cb1d96308ebc2394a35aa2c453
Arthur Morais <mlkliso4000@gmail.com>
2025-03-31 08:02:35 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch_Case/Mês do ano.js
@@ -0,0 +1,42 @@
+let mês = 12;
+
+switch (mês) {
+  case mês = 1:
+    console.log("Janeiro!");
+    break;
+  case mês = 2:
+    console.log("Fevereiro!");
+    break;
+  case mês = 3:
+    console.log("Março!");
+    break;
+  case mês = 4:
+    console.log("Abril!");
+    break;
+  case mês = 5:
+    console.log("Maio!");
+    break;
+  case mês = 6:
+    console.log("Junho!");
+    break;
+  case mês = 7:
+    console.log("Julho!");
+    break;
+  case mês = 8:
+    console.log("Agosto!");
+    break;
+  case mês = 9:
+    console.log("Setembro!");
+    break;
+  case mês = 10:
+    console.log("Outubro!");
+    break;
+  case mês = 11:
+    console.log("Novembro!");
+    break;
+  case mês = 12:
+    console.log("Dezembro!");
+    break;
+  default:
+    console.log("Número inválido! Insira um número de 1 a 12.");
+}
