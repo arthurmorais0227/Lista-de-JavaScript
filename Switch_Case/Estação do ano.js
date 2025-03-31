db265021eb94ac35d50737e3da5998c1bf5a1c0d
Arthur Morais <mlkliso4000@gmail.com>
2025-03-31 08:15:19 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/Estação do ano.js
@@ -0,0 +1,18 @@
+let estação = 5;
+
+switch (estação) {
+  case estação = 1:
+    console.log("Verão! ☀️");
+    break;
+  case estação = 2:
+    console.log("Outono! 🍁");
+    break;
+  case estação = 3:
+    console.log("inverno! ❄️");
+    break;
+  case estação = 4:
+    console.log("Primavera! 🌼");
+    break;
+  default:
+    console.log("Número inserido inválido! Insira um número de 1 a 4.");
+}
